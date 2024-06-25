0a7734c03ddca2452bddf87148b29cf1275f6224
Mohammed Alsumary <139048440+Alsumary@users.noreply.github.com>
2024-06-25 18:50:40 +0300

    Update settings.py

--- aliAlsumary/settings.py
+++ aliAlsumary/settings.py
@@ -119,9 +119,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
 
 # Default primary key field type
