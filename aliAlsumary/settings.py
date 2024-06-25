5e0451fca89699566aa13d701869b8dfdaa36339
Mohammed Alsumary <139048440+Alsumary@users.noreply.github.com>
2024-06-25 19:46:10 +0300

    Update settings.py

--- aliAlsumary/settings.py
+++ aliAlsumary/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3k$d=9dv@uua2#mq50o16csm_hmullq1t@1@qyv(#em(z)&deu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['alsumary.com']
 
