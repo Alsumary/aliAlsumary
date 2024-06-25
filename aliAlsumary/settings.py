ba47d117ef4a9cbfea29c2945e282d3a9f3027da
Mohammed Alsumary <139048440+Alsumary@users.noreply.github.com>
2024-06-25 18:11:05 +0300

    Update settings.py

--- aliAlsumary/settings.py
+++ aliAlsumary/settings.py
@@ -120,7 +120,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'aliPortfolio/static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Default primary key field type
